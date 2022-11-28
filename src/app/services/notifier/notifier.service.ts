58af463359e947da48028c69d83d9ea4272991b0
Dheeraj <dheeraj.sharma@oodles.io>
2022-11-28 19:24:52 +0530

    toastr service added

--- src/app/services/notifier/notifier.service.ts
+++ src/app/services/notifier/notifier.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotifierService {
 
-  constructor() { }
+  constructor(
+    private toastr: ToastrService
+  ) { }
 
   showSuccess(text: string, title = 'Success'): void {
-    // this.toastr.info(text, title);
+    this.toastr.info(text, title);
 
   }
   showError(text: string, title = 'Error'): void {
-    // this.toastr.error(text, title);
+    this.toastr.error(text, title);
   }
 
   showInfo(text: string, title = 'Info'): void {
-    // this.toastr.info(text, title);
+    this.toastr.info(text, title);
   }
 }
