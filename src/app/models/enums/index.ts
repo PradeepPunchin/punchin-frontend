ee9f68f1eb64ae3920cb99d8ae8818f9dcf09b03
Dheeraj <dheeraj.sharma@oodles.io>
2022-12-02 12:28:34 +0530

    login code optimize done

--- src/app/models/enums/index.ts
+++ src/app/models/enums/index.ts
@@ -1,14 +1,23 @@
 
-export enum STORAGE_TOKEN_ENUM {
+export enum STORAGETOKENENUM {
     token = "PUNCHIN_TOKEN",
-    email = "PUNCHIN_EMAIL"
+    userId = "PUNCHIN_USERID",
+    role = "PUNCHIN_USER_ROLE"
 }
 
 export enum ROLES {
     admin = "ADMIN",
     banker = "BANKER",
     verifier = "VERIFIER"
+}
 
+export enum USERSTATUS {
+    active = "ACTIVE",
+    inactive = "INACTIVE"
+}
 
-
+export enum PLATFORM {
+    web = "WEB",
+    android = "ANDROID",
+    ios = "IOS"
 }
--- src/app/models/response/auth.response.ts
+++ src/app/models/response/auth.response.ts
@@ -1,8 +1,24 @@
-export class BaseResponse {
-    timeStamp: string = '';
-    data: any;
-    message: string = '';
-    isSuccess: boolean = false;
-    statusCode: number = 0;
-    constructor() { }
+import { PLATFORM, ROLES, USERSTATUS } from "../enums"
+
+export interface ILoginResponse {
+    id: number,
+    user: IUser,
+    authToken: string,
+    startTime: number,
+    platform: PLATFORM,
+    deviceId: string,
+    lastActiveTime: number,
+    mobile: boolean
 }
+
+export interface IUser {
+    id: number,
+    userId: string,
+    firstName: string,
+    lastName: string,
+    status: USERSTATUS,
+    password: null,
+    role: ROLES,
+    accountLocked: boolean
+}
+
