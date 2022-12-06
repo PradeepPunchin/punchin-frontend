1d7fb46c8eea8e0c4028b19f284c218f202083a5
Shikhar Gupta <shikhar.gupta@oodles.io>
2022-12-06 15:43:24 +0530

    code merge with dev

---
+++ src/app/shared/modals/document-reject-modal/document-reject-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-reject-modal',
+  templateUrl: './document-reject-modal.component.html',
+  styleUrls: ['./document-reject-modal.component.scss']
+})
+export class DocumentRejectModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { DirectivesModule } from '../directives/directives.module';
+
 import { NoDataFoundComponent } from './no-data-found/no-data-found.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
-import { DirectivesModule } from '../directives/directives.module';
-
-
+import { DocumentVerificationRequestModalComponent } from './modals/document-verification-request-modal/document-verification-request-modal.component';
+import { DocumentRejectModalComponent } from './modals/document-reject-modal/document-reject-modal.component';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
 
 @NgModule({
   declarations: [
@@ -23,13 +26,16 @@ import { DirectivesModule } from '../directives/directives.module';
   imports: [
     CommonModule,
     RouterModule,
-    DirectivesModule
+    DirectivesModule,
+    PdfViewerModule
   ],
   exports: [
     NoDataFoundComponent,
     FooterComponent,
     TopNavComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    DocumentVerificationRequestModalComponent,
+    PdfViewerComponent
   ]
 })
 export class SharedModule { }
