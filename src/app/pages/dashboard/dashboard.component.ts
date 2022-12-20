6f8feb55d2d30002ddccc1e73e4e5b817759d6aa
nitesh089 <nitesh.kumar@oodles.io>
2022-12-20 10:09:46 +0530

    merge code

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -169,11 +169,9 @@ export class DashboardComponent implements OnInit {
   downloadMisReport() {
     this.apiService.getDownloadMisReport(this.bankerData).subscribe((res: any) => {
       if (res?.isSuccess) {
-        console.log(res.data, "res");
-
-        // var link = document.createElement("a")
-        // link.href = res.data
-        // link.click()
+        var link = document.createElement("a")
+        link.href = res.data
+        link.click()
       }
     }, (error: any) => {
       this.notifierService.showError(error?.error?.message || "Something went wrong");
