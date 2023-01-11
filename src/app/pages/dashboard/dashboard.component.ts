b5f7f605e28b8ac609231b92f059f84256f9e4e5
nitesh089 <nitesh.kumar@oodles.io>
2023-01-11 11:20:21 +0530

    fixed bug

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -84,6 +84,7 @@ export class DashboardComponent implements OnInit {
   remarkDetails: any[] = [];
   remarkform!: FormGroup
   remarkType: any = 'AGENT'
+  isUnmappedData: boolean = false;
 
 
 
@@ -658,15 +659,20 @@ export class DashboardComponent implements OnInit {
 
   //rejected MIS 
   downloadRejectedMIS() {
+    this.isUnmappedData = true
     this.apiService.getDownloadRejectMISReport().subscribe((res: any) => {
       if (res?.isSuccess && res?.data) {
         window.location.href = res?.data
+        this.isUnmappedData = false
         this.notifierService.showSuccess(res?.message);
       } else {
         this.notifierService.showError("No data found");
+        this.isUnmappedData = false
       }
     }, (error: any) => {
       this.notifierService.showError(error?.error?.message || "Something went wrong");
+      this.isUnmappedData = false
+
     });
   }
 }
